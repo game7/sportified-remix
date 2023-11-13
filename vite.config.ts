ea5f532d45d740f53603661eac87be53fc8139c7
game7 <cmwoodall@yahoo.com>
2023-11-13 21:44:43 +0000

    use vite

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+      // appDirectory: "app",
+      // assetsBuildDirectory: "public/build",
+      // publicPath: "/build/",
+      // serverBuildPath: "build/index.js",
+    }),
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
